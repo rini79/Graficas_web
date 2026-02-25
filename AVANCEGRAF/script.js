3e1478d24e02794d55c3e9bcc4f0e78a30243c5c
rini79 <149265729+rini79@users.noreply.github.com>
2026-02-24 23:27:28 -0600

    pantalla de gameover y victory, login y unos cambios mas

--- AVANCEGRAF/script.js
+++ AVANCEGRAF/script.js
@@ -1,12 +1,17 @@
 let currentScreen = "menu";
 let gamePaused = false;
+let score = 0; // Variable para llevar el control de los puntos
 
 function showScreen(id) {
   document.querySelectorAll(".screen").forEach(s => {
     s.classList.remove("active");
   });
-  document.getElementById(id).classList.add("active");
-  currentScreen = id;
+  
+  const targetScreen = document.getElementById(id);
+  if (targetScreen) {
+    targetScreen.classList.add("active");
+    currentScreen = id;
+  }
 }
 
 function pauseGame() {
@@ -16,5 +21,29 @@ function pauseGame() {
 
 function resumeGame() {
   gamePaused = false;
+  showScreen("game");
+}
+
+function gameOver(finalScore) {
+  gamePaused = true;
+
+  const scoreDisplay = document.getElementById("final-score");
+  if (scoreDisplay) {
+    scoreDisplay.innerText = finalScore;
+  }
+  showScreen("gameover");
+}
+
+
+function victory() {
+  gamePaused = true;
+  showScreen("victory");
+}
+
+
+function restartGame() {
+  score = 0;
+  gamePaused = false;
+
   showScreen("game");
 }
